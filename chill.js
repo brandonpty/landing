98275123550afb62bed8d0075511da70a89d6c2f
BRANDON <brandon@strivelatam.com>
2024-12-01 17:19:17 -0500

    Update chill.js

--- chill.js
+++ chill.js
@@ -1,3 +1,14 @@
 function toggleMode() {
-    document.body.classList.toggle('light-mode');
+  document.body.classList.toggle('light-mode');
+}
+
+function makeBold() {
+  let textarea = document.getElementById("notepad");
+  let selectedText = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+  if (selectedText) {
+    let newText = textarea.value.substring(0, textarea.selectionStart) + 
+                   "<b>" + selectedText + "</b>" + 
+                   textarea.value.substring(textarea.selectionEnd);
+    textarea.value = newText;
+  }
 }
